5b1afa28d1a43203d0e808b55c80de3388451e0e
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2024-02-29 20:25:19 +0200

    feat: add GameOver layer upon player's victory

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,11 +1,12 @@
 import { GameBoard } from "./GameBoard";
+import { GameOver } from "./GameOver";
 import { Players } from "./Players";
 
 export function GameContainer({ activePlayer, board, turns, onPlay, winner }) {
   return (
     <div id="game-container">
       <Players activePlayer={winner ? null : activePlayer} />
-      {winner && <p>You are the winner: {winner}!</p>}
+      {winner && <GameOver winner={winner} />}
       <GameBoard board={board} turns={turns} onPlay={onPlay} winner={winner} />
     </div>
   );
---
+++ src/components/GameOver.jsx
@@ -0,0 +1,9 @@
+export function GameOver({ winner }) {
+  return (
+    <div id="game-over">
+      <h2>Game Over</h2>
+      <p>{winner} won!</p>
+      <button>Rematch!</button>
+    </div>
+  );
+}
