ba75155e26d76699cb0a86358600e2eea8ad7dfe
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2024-02-29 20:20:21 +0200

    feat: remove active player selection upon player's victory

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -4,7 +4,7 @@ import { Players } from "./Players";
 export function GameContainer({ activePlayer, board, turns, onPlay, winner }) {
   return (
     <div id="game-container">
-      <Players activePlayer={activePlayer} />
+      <Players activePlayer={winner ? null : activePlayer} />
       {winner && <p>You are the winner: {winner}!</p>}
       <GameBoard board={board} turns={turns} onPlay={onPlay} winner={winner} />
     </div>
